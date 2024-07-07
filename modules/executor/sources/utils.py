f86e0663088bb72de99b597b47c43640059348ae
root <root@localhost.localdomain>
2024-07-07 21:56:56 +0200

    terminal 2 lines (fix 2)

--- modules/executor/sources/utils.py
+++ modules/executor/sources/utils.py
@@ -60,7 +60,6 @@ class Stream:
             else:
                 break
 
-
 class AsyncTerminal:
     def __init__(self, message: Message, command: str, text: str, sleep: int):
         self.command = command
